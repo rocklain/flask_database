1cb7ced93545fefa265386d53ad0e157e3aece74
rocklain <ztgk.3142@gmail.com>
2025-07-23 00:02:37 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, email, username, password_hash):
+        self.email = email
+        self.username = username
+        self.password_hash = password_hash
+
+    def __repr__(self):
+        return f"UserName: {self.username}"
+    
+if __name__ == '__main__':
+    app.run(debug=True)
