b3f36dd4fc8f90da65ec600c59744608153573bd
rocklain <ztgk.3142@gmail.com>
2025-07-23 21:17:59 +0900

    Flask Migrateの実装

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -12,6 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+Migrate(app, db)
 
 
 class User(db.Model):
@@ -21,14 +23,17 @@ class User(db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    administrator = db.Column(db.String(1))
 
-    def __init__(self, email, username, password_hash):
+    def __init__(self, email, username, password_hash, administrator):
         self.email = email
         self.username = username
         self.password_hash = password_hash
+        self.administrator = administrator
 
     def __repr__(self):
         return f"UserName: {self.username}"
-    
+
+
 if __name__ == '__main__':
     app.run(debug=True)
