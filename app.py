a56fb640d161536f6715b580bc6a6c1081b89c8d
rocklain <ztgk.3142@gmail.com>
2025-07-23 21:47:08 +0900

    外部キー制約の有効化

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from datetime import datetime
+from pytz import timezone
 
 app = Flask(__name__)
 
@@ -15,6 +19,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app, db)
 
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -24,6 +34,7 @@ class User(db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     administrator = db.Column(db.String(1))
+    post = db.relationship('BlogPost', backref='author', lazy='dynamic')
 
     def __init__(self, email, username, password_hash, administrator):
         self.email = email
@@ -33,6 +44,27 @@ class User(db.Model):
 
     def __repr__(self):
         return f"UserName: {self.username}"
+
+
+class BlogPost(db.Model):
+    __tablename__ = 'blog_post'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    date = db.Column(db.DateTime, default=datetime.now(timezone('Asia/Tokyo')))
+    title = db.Column(db.String(140))
+    text = db.Column(db.Text)
+    summary = db.Column(db.String(140))
+    featured_image = db.Column(db.String(140))
+
+    def __init__(self, title, text, featured_image, user_id, summary):
+        self.title = title
+        self.text = text
+        self.featured_image = featured_image
+        self.user_id = user_id
+        self.summary = summary
+
+    def __repr__(self):
+        return f"PostID: {self.id}, Title: {self.title}, Author: {self.author} \n"
 
 
 if __name__ == '__main__':
